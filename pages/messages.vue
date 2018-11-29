d619e30ef98f3fbe35b29cd8d1bf6bd074571dd2
Akinyele Olubodun <akinyeleolubodun@gmail.com>
2018-11-29 20:36:20 +0100

    Adding messaging view

--- pages/messages.vue
+++ pages/messages.vue
@@ -1,165 +1,122 @@
+
 <template>
-<section>
-  <v-container fluid>
-    <v-layout mb-5>
-      <div class="headline">Messages</div> &nbsp;&nbsp;&nbsp;&nbsp; <div class="headline primary--text">6</div>
-      <v-layout class="justify-end">
-        <v-btn to="/new-message" class="primary caption">
-          <v-icon>add</v-icon> NEW MESSAGE
-        </v-btn>
+  <section>
+    <v-container fluid>
+      <v-layout mb-5>
+        <div class="headline" style="margin-right:5px;">Messages</div>
+        <div class="headline primary--text">({{messages_count}})</div>
+        <v-layout class="justify-end">
+          <v-btn to="/new-message" class="primary caption">
+            <v-icon>add</v-icon>NEW MESSAGE
+          </v-btn>
+        </v-layout>
       </v-layout>
-    </v-layout>
 
-    <v-layout row wrap pl-5>
-      <v-flex md8>
-        <v-btn-toggle v-model="timeSort" class="mb-2">
-          <v-btn flat value="day" class="caption multiple-btn">
-            DAY
-          </v-btn>
-          <v-btn flat value="month" class="caption multiple-btn">
-            MONTH
-          </v-btn>
-          <v-btn flat value="year" class="caption multiple-btn">
-            YEAR
-          </v-btn>
-        </v-btn-toggle>
-      </v-flex>
+      <v-card>
+        <v-card-title></v-card-title>
+        <v-data-table :headers="headers" :items="messages" :search="search">
+          <template slot="no-data" class="text-xs-center">Loading...</template>
+          <template slot="items" slot-scope="props">
+            <tr>
+              <td>{{ props.item.status }}</td>
+              <td>{{ props.item.recipients }}</td>
+              <td>{{ props.item.reward }}</td>
+              <td>{{ props.item.body }}</td>
+              <td style="width:12%">{{ props.item.date }}</td>
+            </tr>
+          </template>
+          <v-alert
+            slot="no-results"
+            :value="true"
+            color="error"
+            icon="warning"
+          >Your search for "{{ search }}" found no results.</v-alert>
+        </v-data-table>
+      </v-card>
 
-      <v-flex md4>
-        <v-btn-toggle v-model="statusSort" class="mb-2">
-          <v-btn flat value="all" class="caption multiple-btn">
-            ALL
-          </v-btn>
-          <v-btn flat value="sent" class="caption multiple-btn">
-            SENT
-          </v-btn>
-          <v-btn flat value="scheduled" class="caption multiple-btn">
-            SCHEDULED
-          </v-btn>
-        </v-btn-toggle>
-      </v-flex>
-    </v-layout>
+  <v-expansion-panel>
+      <v-expansion-panel-content
+        v-for="(x, i) in messagesMonth"
+        :key="i"
+      >
+        <div slot="header">{{x}}</div>
+        <v-card>
+          <v-data-table :headers="headers" :items="messages" :search="search">
+            <template slot="no-data" class="text-xs-center">Loading...</template>
+            <template slot="items" slot-scope="props">
+              <tr>
+                <td>{{ props.item.status }}</td>
+                <td>{{ props.item.recipients }}</td>
+                <td>{{ props.item.reward }}</td>
+                <td>{{ props.item.body }}</td>
+                <td style="width:12%">{{ props.item.date }}</td>
+              </tr>
+            </template>
+            <v-alert
+              slot="no-results"
+              :value="true"
+              color="error"
+              icon="warning"
+            >Your search for "{{ search }}" found no results.</v-alert>
+          </v-data-table>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
 
-    <v-card>
-      <v-card-title>
-      </v-card-title>
-      <v-data-table :headers="headers" :items="messages" :search="search">
-        <template slot="items" slot-scope="props">
-          <tr>
-            <td>{{ props.item.status }}</td>
-            <td>{{ props.item.recipients }}</td>
-            <td>{{ props.item.reward }}</td>
-            <td>{{ props.item.message }}</td>
-            <td>{{ props.item.date }}</td>
-            <td class="justify-center layout px-0">
-              <v-flex mt-2 class="text-xs-center">
-                <v-btn icon to="/message-detail">
-                  <v-icon small class="green--text">visibility</v-icon>
-                </v-btn>
-              </v-flex>
-            </td>
-          </tr>
-        </template>
-        <v-alert slot="no-results" :value="true" color="error" icon="warning">
-          Your search for "{{ search }}" found no results.
-        </v-alert>
-      </v-data-table>
-    </v-card>
-  </v-container>
-</section>
+    </v-container>
+  </section>
+  
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  layout: 'dashboard',
-  data() {
-    return {
-      timeSort: 'day',
-      statusSort: 'sent',
-      search: '',
-      headers: [{
-          text: 'Status',
-          align: 'left',
-          value: 'status'
-        },
-        {
-          text: 'Recipients',
-          value: 'recipients'
-        },
-        {
-          text: 'Reward',
-          value: 'reward'
-        },
-        {
-          text: 'Message',
-          value: 'message'
-        },
-        {
-          text: 'Date',
-          value: 'date'
-        },
-        {
-          text: 'Actions',
-          value: 'name',
-          sortable: false
+	
+	layout: 'dashboard',
+	
+	data() {
+		
+		return {
+			
+			timeSort: 'day',
+			statusSort: 'all',
+			search: '',
+			headers: [
+				{text: 'Status', align: 'left', value: 'status' },
+				{text: 'Recipients', value: 'recipients' },
+				{text: 'Reward', value: 'reward'},
+				{text: 'Message', value: 'message' },
+				{text: 'Date', value: 'date' }
+			],
+      messages : [],
+      messages_count : 0
+		}
+	},
+	created : async function(){
+			try{
+				let payload = { headers : {'Content-Type' : 'application/json', 'Authorization' : 'Bearer 2f66686be77a3eff684cead289fabe873c8032dfaf8a3fc8c13b4a6dd26c2b89'} }
+        let response = await axios.get('https://theelect-smsapi.herokuapp.com/index.php/api/messages/all', payload);
+				
+        let {status, data} = response.data;
+
+				if(status != false){
+					
+					this.messages = data;
+					this.messages_count = data.length;
         }
-      ],
-      messages: [{
-          value: false,
-          status: 'Scheduled',
-          recipients: 159,
-          reward: '₦6.0',
-          message: 'Hey My name is Edo Williams. Please vote for me today!',
-          date: 'November 1st, 2018'
-        },
-        {
-          value: false,
-          status: 'Sent',
-          recipients: 237,
-          reward: '₦9.0',
-          message: 'Hey My name is Edo Williams. Please vote for me today!',
-          date: 'November 1st, 2018'
-        },
-        {
-          value: false,
-          status: 'Scheduled',
-          recipients: 262,
-          reward: '₦16.0',
-          message: 'Hey My name is Edo Williams. Please vote for me today!',
-          date: 'November 1st, 2018'
-        },
-        {
-          value: false,
-          status: 'Sent',
-          recipients: 305,
-          reward: '₦3.7',
-          message: 'Hey My name is Edo Williams. Please vote for me today!',
-          date: 'November 1st, 2018'
-        },
-        {
-          value: false,
-          status: 'Scheduled',
-          recipients: 356,
-          reward: '₦16.0',
-          message: 'Hey My name is Edo Williams. Please vote for me today!',
-          date: 'November 1st, 2018'
-        },
-        {
-          value: false,
-          status: 'Sent',
-          recipients: 375,
-          reward: '₦0.0',
-          message: 'Hey My name is Edo Williams. Please vote for me today!',
-          date: 'November 1st, 2018'
-        }
-      ]
-    }
-  }
+
+			}catch(ex){
+				console.error(ex);
+			}
+	}
+	
 }
+
 </script>
 
 <style lang="scss" scoped>
 .multiple-btn {
-  width: 100px;
+	width: 100px;
 }
 </style>

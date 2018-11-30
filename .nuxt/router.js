9e9d86e77d3ddf7db908e3bff34b885f64caedcf
Akinyele Olubodun <akinyeleolubodun@gmail.com>
2018-11-30 15:22:33 +0100

    Added new message page

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -2,28 +2,28 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { interopDefault } from './utils'
 
-const _353ad358 = () => interopDefault(import('../pages/admin.vue' /* webpackChunkName: "pages/admin" */))
-const _e4866978 = () => interopDefault(import('../pages/age.vue' /* webpackChunkName: "pages/age" */))
-const _7b0ed3a2 = () => interopDefault(import('../pages/all-voters.vue' /* webpackChunkName: "pages/all-voters" */))
-const _3dc2ee5e = () => interopDefault(import('../pages/contacts.vue' /* webpackChunkName: "pages/contacts" */))
-const _0d66d6ce = () => interopDefault(import('../pages/dashboard.vue' /* webpackChunkName: "pages/dashboard" */))
-const _014962b3 = () => interopDefault(import('../pages/edit-user.vue' /* webpackChunkName: "pages/edit-user" */))
-const _2de435ca = () => interopDefault(import('../pages/forgot-password.vue' /* webpackChunkName: "pages/forgot-password" */))
-const _c53c6d10 = () => interopDefault(import('../pages/lgas.vue' /* webpackChunkName: "pages/lgas" */))
-const _2db8a6a2 = () => interopDefault(import('../pages/message-detail.vue' /* webpackChunkName: "pages/message-detail" */))
-const _b6212a52 = () => interopDefault(import('../pages/messages.vue' /* webpackChunkName: "pages/messages" */))
-const _7e40cc38 = () => interopDefault(import('../pages/new-contact.vue' /* webpackChunkName: "pages/new-contact" */))
-const _47c65c9f = () => interopDefault(import('../pages/new-message.vue' /* webpackChunkName: "pages/new-message" */))
-const _e936a93a = () => interopDefault(import('../pages/new-user.vue' /* webpackChunkName: "pages/new-user" */))
-const _1184030e = () => interopDefault(import('../pages/old-new-message.vue' /* webpackChunkName: "pages/old-new-message" */))
-const _0aa481c7 = () => interopDefault(import('../pages/profession.vue' /* webpackChunkName: "pages/profession" */))
-const _7da4186e = () => interopDefault(import('../pages/profile.vue' /* webpackChunkName: "pages/profile" */))
-const _e1af65dc = () => interopDefault(import('../pages/realtime-voters.vue' /* webpackChunkName: "pages/realtime-voters" */))
-const _1065e22e = () => interopDefault(import('../pages/settings.vue' /* webpackChunkName: "pages/settings" */))
-const _dd37e6c6 = () => interopDefault(import('../pages/test.vue' /* webpackChunkName: "pages/test" */))
-const _24b34550 = () => interopDefault(import('../pages/users/index.vue' /* webpackChunkName: "pages/users/index" */))
-const _4a6d8f8a = () => interopDefault(import('../pages/users/_id/index.vue' /* webpackChunkName: "pages/users/_id/index" */))
-const _98197b52 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _afe7dda4 = () => interopDefault(import('../pages/admin.vue' /* webpackChunkName: "pages/admin" */))
+const _034996c4 = () => interopDefault(import('../pages/age.vue' /* webpackChunkName: "pages/age" */))
+const _c57bddf0 = () => interopDefault(import('../pages/all-voters.vue' /* webpackChunkName: "pages/all-voters" */))
+const _46524d44 = () => interopDefault(import('../pages/contacts.vue' /* webpackChunkName: "pages/contacts" */))
+const _02a91273 = () => interopDefault(import('../pages/dashboard.vue' /* webpackChunkName: "pages/dashboard" */))
+const _0aa5e08d = () => interopDefault(import('../pages/edit-user.vue' /* webpackChunkName: "pages/edit-user" */))
+const _393d6824 = () => interopDefault(import('../pages/forgot-password.vue' /* webpackChunkName: "pages/forgot-password" */))
+const _40908b5e = () => interopDefault(import('../pages/lgas.vue' /* webpackChunkName: "pages/lgas" */))
+const _5fa2c108 = () => interopDefault(import('../pages/message-detail.vue' /* webpackChunkName: "pages/message-detail" */))
+const _a5026c86 = () => interopDefault(import('../pages/messages.vue' /* webpackChunkName: "pages/messages" */))
+const _bb1588dc = () => interopDefault(import('../pages/new-contact.vue' /* webpackChunkName: "pages/new-contact" */))
+const _6bfacbf9 = () => interopDefault(import('../pages/new-message.vue' /* webpackChunkName: "pages/new-message" */))
+const _d817eb6e = () => interopDefault(import('../pages/new-user.vue' /* webpackChunkName: "pages/new-user" */))
+const _029730d3 = () => interopDefault(import('../pages/old-new-message.vue' /* webpackChunkName: "pages/old-new-message" */))
+const _2cd7bf2d = () => interopDefault(import('../pages/profession.vue' /* webpackChunkName: "pages/profession" */))
+const _3bda44c8 = () => interopDefault(import('../pages/profile.vue' /* webpackChunkName: "pages/profile" */))
+const _cafd0128 = () => interopDefault(import('../pages/realtime-voters.vue' /* webpackChunkName: "pages/realtime-voters" */))
+const _18f54114 = () => interopDefault(import('../pages/settings.vue' /* webpackChunkName: "pages/settings" */))
+const _3492ce83 = () => interopDefault(import('../pages/test.vue' /* webpackChunkName: "pages/test" */))
+const _48e7b4aa = () => interopDefault(import('../pages/users/index.vue' /* webpackChunkName: "pages/users/index" */))
+const _33bb2ad6 = () => interopDefault(import('../pages/users/_id/index.vue' /* webpackChunkName: "pages/users/_id/index" */))
+const _769cbd31 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
 
@@ -84,91 +84,91 @@ export function createRouter() {
 
     routes: [{
       path: "/admin",
-      component: _353ad358,
+      component: _afe7dda4,
       name: "admin"
     }, {
       path: "/age",
-      component: _e4866978,
+      component: _034996c4,
       name: "age"
     }, {
       path: "/all-voters",
-      component: _7b0ed3a2,
+      component: _c57bddf0,
       name: "all-voters"
     }, {
       path: "/contacts",
-      component: _3dc2ee5e,
+      component: _46524d44,
       name: "contacts"
     }, {
       path: "/dashboard",
-      component: _0d66d6ce,
+      component: _02a91273,
       name: "dashboard"
     }, {
       path: "/edit-user",
-      component: _014962b3,
+      component: _0aa5e08d,
       name: "edit-user"
     }, {
       path: "/forgot-password",
-      component: _2de435ca,
+      component: _393d6824,
       name: "forgot-password"
     }, {
       path: "/lgas",
-      component: _c53c6d10,
+      component: _40908b5e,
       name: "lgas"
     }, {
       path: "/message-detail",
-      component: _2db8a6a2,
+      component: _5fa2c108,
       name: "message-detail"
     }, {
       path: "/messages",
-      component: _b6212a52,
+      component: _a5026c86,
       name: "messages"
     }, {
       path: "/new-contact",
-      component: _7e40cc38,
+      component: _bb1588dc,
       name: "new-contact"
     }, {
       path: "/new-message",
-      component: _47c65c9f,
+      component: _6bfacbf9,
       name: "new-message"
     }, {
       path: "/new-user",
-      component: _e936a93a,
+      component: _d817eb6e,
       name: "new-user"
     }, {
       path: "/old-new-message",
-      component: _1184030e,
+      component: _029730d3,
       name: "old-new-message"
     }, {
       path: "/profession",
-      component: _0aa481c7,
+      component: _2cd7bf2d,
       name: "profession"
     }, {
       path: "/profile",
-      component: _7da4186e,
+      component: _3bda44c8,
       name: "profile"
     }, {
       path: "/realtime-voters",
-      component: _e1af65dc,
+      component: _cafd0128,
       name: "realtime-voters"
     }, {
       path: "/settings",
-      component: _1065e22e,
+      component: _18f54114,
       name: "settings"
     }, {
       path: "/test",
-      component: _dd37e6c6,
+      component: _3492ce83,
       name: "test"
     }, {
       path: "/users",
-      component: _24b34550,
+      component: _48e7b4aa,
       name: "users"
     }, {
       path: "/users/:id",
-      component: _4a6d8f8a,
+      component: _33bb2ad6,
       name: "users-id"
     }, {
       path: "/",
-      component: _98197b52,
+      component: _769cbd31,
       name: "index"
     }],
 
--- pages/new-message.vue
+++ pages/new-message.vue
@@ -16,7 +16,7 @@
     </v-text-field>
 
     <div class="caption blue-grey--text mt-2">MESSAGE BODY</div>
-    <v-textarea class="mb-4" v-model="messageBody" auto-grow box color="primary" rows="8" counter>
+    <v-textarea class="mb-4" v-model="body" auto-grow box color="primary" rows="8" counter>
       <div class="caption" slot="label">Message</div>
     </v-textarea>
 
@@ -27,48 +27,107 @@
 </template>
 
 <script>
+import moment from "moment";
+let sms_url = "http://theelect-smsapi.herokuapp.com/index.php/api";
+import axios from "axios";
 export default {
-  layout: 'dashboard',
+  layout: "dashboard",
   data() {
     return {
-      recipients: [],
-      messageBody: ''
+      step: 1,
+      sender: "Tonye Cole",
+      recipients: "",
+      date: new Date().toISOString().substr(0, 10),
+      dateMenu: false,
+      body: "",
+      autoUpdate: true,
+      age: [],
+      gender: [],
+      month: [],
+      occupation: [],
+      local: [],
+      locals: [],
+      ward: [],
+      wards: [],
+      ages: [],
+      genders: [],
+      months: [],
+      occupations: [],
+			
+			isUpdating: false,
+			group : "",
+			error : false,
+			error_message : '',
+			success : false,
+			success_message : ''
+		
+		};
+  },
+  computed: {
+    sendDate() {
+      return this.date ? moment(this.date).format("MMMM Do YYYY") : "";
+    }
+  },
+  watch: {
+    isUpdating(val) {
+      if (val) {
+        setTimeout(() => (this.isUpdating = false), 3000);
+      }
     }
   },
   methods: {
-    async sendMessage() {
-      this.$toast.show('Sending...', {
-        icon: "fingerprint"
-      });
-      const data = {
-        recipients: this.recipients,
-        body: this.messageBody,
-        sender : "Street",
-        recipients_type : "custom",
-        age_bracket : [],
-        marital_status : [],
-        birth_month : [],
-        groups : [],
-        states : [],
-        locals : []
-      }
-      await this.$axios.post('https://theelect-smsapi.herokuapp.com/index.php/api/message?api_token=2f66686be77a3eff684cead289fabe873c8032dfaf8a3fc8c13b4a6dd26c2b89', data, {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      }).then(response => {
-        this.$toast.success('Message sent successfully', {
-          icon: "check"
-        });
-        this.$router.push(`/messages`);
-      }).catch(e => {
-        console.log(e);
-      })
-      console.log(data)
-    }
+    remove(item) {
+      const index = this.friends.indexOf(item.name);
+      if (index >= 0) this.friends.splice(index, 1);
+    },
+    async submit() {
+			this.group = `Ages ${this.age.join(', ')}, Genders ${this.genders.join(', ')}, Professions ${this.occupation.join(', ')}`;
+			this.step = 2;
+      
+		},
+		async sendMessage(){
+			try{
+				this.error = this.success = false;
+				
+				let body = {
+					body : this.body,
+					recipients_type : 'custom',
+					scheduled : 0,
+					schedule_date : this.date,
+					sender : this.sender,
+					ages : this.age,
+					gender : this.gender,
+					locals : this.local,
+					wards : this.ward,
+					occupations : this.occupation,
+          months : this.month,
+          recipients: this.recipients.split(',')
+				}
+				let response = await axios.post(`${sms_url}/message?api_token=2f66686b`, body);
+				
+				let {status, data} = response.data;
+				
+				if(status == false){
+					this.error = true;
+					this.step = 1;
+					this.error_message = data;
+				
+				}else{
+					this.success = true;
+					this.step = 1;
+					this.success_message = 'Message has been queued successfully.'
+					setTimeout(() => this.$router.push(`/messages`), 2000)
+				}
+				
+			}catch(ex){
+				this.error = true;
+				this.step = 1;
+				this.error_message = ex.message.toString()
+			}
+		}
+	
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>

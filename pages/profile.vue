14517fad780e01adfc67fa7b75e4db5c47475177
Abbey <i.olasunkanmi@dreammesh.ng>
2019-01-17 05:18:38 +0100

    Revamped the profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -13,25 +13,27 @@
       </v-card>
     </v-dialog>
 
-    <v-layout justify-center my-5>
-      <v-card width="400" class="text-xs-center pa-5 primary white--text curved-card">
-        <v-avatar size="140">
-          <v-img :src="`/tonye.jpg`">
-            <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
-              <v-progress-circular indeterminate color="primary"></v-progress-circular>
-            </v-layout>
-          </v-img>
-        </v-avatar>
+    <v-sheet width="80%" class="pa-5 mx-auto curved-card my-4" color="grey lighten-3">
+      <v-layout justify-center my-5>
+        <v-card width="400" class="text-xs-center pa-5 primary white--text curved-card elevation-12">
+          <v-avatar size="140">
+            <v-img :src="`/tonye.jpg`">
+              <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+              </v-layout>
+            </v-img>
+          </v-avatar>
 
-        <div class="headline font-weight-bold mt-4">{{ user.first_name + ' ' + user.last_name}}</div>
-        <div class="font-weight-regular mb-4">{{ user.role | capitalize }}</div>
-        <v-divider></v-divider>
-        <div class="title font-weight-regular mt-4">{{ user.email }}</div>
-        <div class="title font-weight-regular mt-3 mb-5">{{ user.phone }}</div>
+          <div class="headline font-weight-bold mt-4">{{ user.first_name + ' ' + user.last_name}}</div>
+          <div class="font-weight-regular mb-4">{{ user.role | capitalize }}</div>
+          <v-divider></v-divider>
+          <div class="title font-weight-regular mt-4">{{ user.email }}</div>
+          <div class="title font-weight-regular mt-3 mb-5">{{ user.phone }}</div>
 
-        <v-btn @click="dialog = true" outline dark class="white">Edit Profile</v-btn>
-      </v-card>
-    </v-layout>
+          <v-btn @click="dialog = true" outline dark class="white">Edit Profile</v-btn>
+        </v-card>
+      </v-layout>
+    </v-sheet>
   </v-container>
 </section>
 </template>
